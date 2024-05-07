625a392cac2ce1123f4d14af35864aa95fd6b40d
fatemeh <fatemehdordkeshan@yahoo.com>
2024-05-07 20:07:02 +0330

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    boxShadow: {
+      '3xl': '0 20px 25px -5px rgb(0 0 0 / 0.4), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
